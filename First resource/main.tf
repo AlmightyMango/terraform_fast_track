9203c6d023455abedf7710c1b41f16ec3ffc4978
Apth <17009502+AlmightyMango@users.noreply.github.com>
2023-03-29 03:34:45 -0500

    fix

--- First resource/main.tf
+++ First resource/main.tf
@@ -1,11 +1,3 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-  
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
 provider "aws" {
     region = "us-east-1"
 }
